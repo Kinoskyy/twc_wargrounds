eee4ce6b5e4da5ff022ce825eed9110712964411
Kinoskyy <vergadepuerko666@gmail.com>
2025-03-30 10:05:24 -0600

    Update AC_S.lua

--- wargrounds/server/AC_S.lua
+++ wargrounds/server/AC_S.lua
@@ -8,11 +8,17 @@ function getAdmins()
     return admins
 end
 
+local function removeColorCodes(name)
+    if type(name) ~= "string" then return name end
+    return name:gsub("#%x%x%x%x%x%x", "")
+end
+
 addEvent("suspectSpeed", true)
 addEventHandler("suspectSpeed", root, function(speed)
     if not client or not isElement(client) then return end
     local formattedSpeed = string.format("%.2f", speed)
+    local playerName = removeColorCodes(getPlayerName(client))
     for k, v in pairs(getAdmins()) do
-        outputChatBox(getPlayerName(client) .. " Suspect sprint macro " .. formattedSpeed .. " km/h", v, 255, 0, 0)
+        outputChatBox(playerName .. " suspect sprint macro " .. formattedSpeed .. " KM/H", v, 255, 0, 0)
     end
 end)
