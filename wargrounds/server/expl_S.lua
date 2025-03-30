f48117d59a1378a459714d0763ac03220193cf23
Kinoskyy <vergadepuerko666@gmail.com>
2025-03-30 10:31:46 -0600

    Delete wargrounds/server/expl_S.lua

--- wargrounds/server/expl_S.lua
+++
@@ -1,6 +0,0 @@
-addEventHandler( "onPlayerWasted", getRootElement(), 
-function(_,killer) 
-	if(killer) then playSoundFrontEnd ( killer,40 ) end	
-	local x,y,z = getElementPosition(source)
-	triggerClientEvent("explodeBody", getRootElement(), x,y,z)
-end)
