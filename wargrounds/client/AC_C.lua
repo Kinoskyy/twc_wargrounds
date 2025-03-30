84680a1993b091791a1c51ce411c2a0c11d4c6d3
Kinoskyy <vergadepuerko666@gmail.com>
2025-03-30 10:42:22 -0600

    Update AC_C.lua

--- wargrounds/client/AC_C.lua
+++ wargrounds/client/AC_C.lua
@@ -1,9 +1,3 @@
-addEvent("requestScreenMode", true)
-addEventHandler("requestScreenMode", localPlayer, function(requestingPlayer)
-    local screenMode = dxGetStatus().SettingWindowed and "Windowed" or "Fullscreen"
-    local fullscreenMode = dxGetStatus().SettingFullScreenStyle == 0 and 'Fullscreen' or 'Windowed'
-    triggerServerEvent("sendScreenMode", resourceRoot, fullscreenMode)
-end)
 function getKeys( control )
     local keys = { }; 
     local boundKeys = getBoundKeys( control ) 
@@ -14,12 +8,12 @@ function getKeys( control )
 end 
 function getPlayerSpeed( player )
     local speedX, speedY, speedZ = getElementVelocity( player )
-    local speed = math.sqrt( speedX^2 + speedY^2 + speedZ^2 ) * 180; -- km/h
+    local speed = math.sqrt( speedX^2 + speedY^2 + speedZ^2 ) * 180;
     return speed
 end 
 addEventHandler( 'onClientKey', root, function ( key, state )
     local keys = {jump = getKeys( 'jump'),fire = getKeys( 'fire'), aim = getKeys( 'aim_weapon'), sprint = getKeys( 'sprint')  }
-    local states = { jump = getControlState('jump'), aim = getControlState('aim_weapon'), fire = getControlState('fire'), sprint = getControlState('sprint') }
+    local states = { jump = getPedControlState('jump'), aim = getPedControlState('aim_weapon'), fire = getPedControlState('fire'), sprint = getPedControlState('sprint') }
     if( keys.sprint[key] == 'down' and (not states.aim) ) then 
         local speed = getPlayerSpeed( localPlayer )
         if( speed > 55 and isPedOnGround( localPlayer)) then 
